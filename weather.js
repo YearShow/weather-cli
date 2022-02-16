12920a7406ff2e6d786632373671fad64fd87c3a
Alexander Ershov <yearshow@yandex.ru>
2022-02-16 15:23:19 +0300

    added API service by 'https' standard nodeJS module

--- weather.js
+++ weather.js
@@ -1,14 +1,15 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
+import { getWeather } from './services/api.service.js';
 import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue } from './services/storage.servece.js';
+import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
 		printError('Не передан токен');
 	}
 	try {
-		await saveKeyValue('token', token);
+		await saveKeyValue(TOKEN_DICTIONARY.token, token);
 		printSuccess('Токен сохранён');
 	} catch (e) {
 		printError(e.message);
@@ -26,7 +27,7 @@ const initCLI = () => {
 	if (args.t) {
 		return saveToken(args.t);
 	}
-	// Вывести погоду
+	getWeather('tver');
 
 };
 
