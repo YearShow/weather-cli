a5dbf9c121c85203112fd399eafae0385c437ba7
Alexander Ershov <yearshow@yandex.ru>
2024-04-07 11:06:23 +0300

    add printWeather service

--- weather.js
+++ weather.js
@@ -1,12 +1,13 @@
 #!/usr/bin/env node
 import { getArgs } from './helpers/args.js';
-import { getWeather } from './services/api.service.js';
-import { printHelp, printSuccess, printError } from './services/log.service.js';
-import { saveKeyValue, TOKEN_DICTIONARY } from './services/storage.service.js';
+import { getWeather, getIcon } from './services/api.service.js';
+import { printHelp, printSuccess, printError, printWeather } from './services/log.service.js';
+import {getKeyValue, saveKeyValue, TOKEN_DICTIONARY} from './services/storage.service.js';
 
 const saveToken = async (token) => {
 	if (!token.length) {
 		printError('Не передан токен');
+		return;
 	}
 	try {
 		await saveKeyValue(TOKEN_DICTIONARY.token, token);
@@ -16,14 +17,28 @@ const saveToken = async (token) => {
 	}
 }
 
+const saveCity = async (city) => {
+	if (!city.length) {
+		printError('Не передан город');
+		return;
+	}
+	try {
+		await saveKeyValue(TOKEN_DICTIONARY.city, city);
+		printSuccess('Город сохранён');
+	} catch (e) {
+		printError(e.message);
+	}
+}
+
 const getForcast = async () => {
 	try {
-		const weather = await getWeather(process.env.CITY);
-		console.log(weather);
+		const city = process.env.CITY ?? await getKeyValue(TOKEN_DICTIONARY.city);
+		const weather = await getWeather(city);
+		printWeather(weather, getIcon(weather.weather[0].icon));
 	} catch (e) {
-		if (e?.response?.status == 404) {
+		if (e?.response?.status === 404) {
 			printError('Неверно указан город');
-		} else if (e?.response?.status == 401) {
+		} else if (e?.response?.status === 401) {
 			printError('Неверно указан токен');
 		} else {
 			printError(e.message);
@@ -33,17 +48,17 @@ const getForcast = async () => {
 
 const initCLI = () => {
 	const args = getArgs(process.argv);
-	console.log(process.env);
+
 	if (args.h) {
-		printHelp();
+		return printHelp();
 	}
 	if (args.s) {
-		// Сохранить город
+		return saveCity(args.s)
 	}
 	if (args.t) {
 		return saveToken(args.t);
 	}
-	getForcast();
+	return getForcast();
 };
 
 initCLI();
